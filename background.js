0c0f0056f3b0fab8f61c5b6c1eb21b289624e674
Ryder <155456337+ryderme@users.noreply.github.com>
2024-01-12 15:04:54 +0800

    新增contextMenu跳转ins的功能
    右键超链接，在contextMenu中加入Media Leap的item

--- background.js
+++ background.js
@@ -1,6 +1,33 @@
-chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
-  if (request.message === "getTwitterUsername") {
-    let link = "https://instagram.com/" + request.username
-    console.log(link)
+/**
+ * 废弃功能
+ */
+// chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
+//   if (request.message === "getTwitterUsername") {
+//     let link = "https://instagram.com/" + request.username
+//     console.log(link)
+//   }else if(request.message === "openTab"){
+//     chrome.tabs.create({ url: "https://www.google.com" });
+//   }
+// })
+
+
+chrome.runtime.onInstalled.addListener(() => {
+  chrome.contextMenus.create({
+    id: "mediaLeap",
+    title: "Media Leap",
+    contexts: ["link"]
+  });
+});
+
+chrome.contextMenus.onClicked.addListener((info, tab) => {
+  if (info.menuItemId === "mediaLeap") {
+    let twitterUsername = extractTwitterUsername(info.linkUrl)
+    let instagramUrl = `https://instagram.com/${twitterUsername}`;
+    chrome.tabs.create({ url: instagramUrl });
   }
-})
+});
+
+function extractTwitterUsername(url) {
+  let match = url.match(/twitter\.com\/([a-zA-Z0-9_]+)/);
+  return match ? match[1] : '';
+}
--- content.js
+++ content.js
@@ -1,4 +1,54 @@
-chrome.runtime.sendMessage({
-    message: "getTwitterUsername",
-    username: window.location.pathname.slice(1)
-})
+
+/**
+ * 废弃功能：选中文字，提取username，弹出快捷icon
+ */
+// document.addEventListener('mouseup', function (e) {
+//     // 检查是否点击的是图标本身
+//     if (e.target && e.target.id === 'myExtensionIcon') {
+//         return; // 如果是点击图标，则不执行后续逻辑
+//     }
+//     const selectedText = window.getSelection().toString().trim();
+//     if (selectedText) {
+//         showIconAtCursor(e.clientX, e.clientY);
+//     }
+// });
+
+// function showIconAtCursor(x, y) {
+//     const icon = document.createElement('img');
+//     icon.src = chrome.runtime.getURL('images/icon-32.png');
+//     icon.style.position = 'fixed';
+//     icon.style.left = `${x}px`;
+//     icon.style.top = `${y}px`;
+//     icon.style.cursor = 'pointer';
+//     icon.style.zIndex = '1000'; // 确保图标位于最上层
+//     icon.id = 'myExtensionIcon';
+
+//     // 删除之前可能存在的图标
+//     const existingIcon = document.getElementById('myExtensionIcon');
+//     if (existingIcon) {
+//         document.body.removeChild(existingIcon);
+//     }
+//     document.body.appendChild(icon);
+
+//     icon.addEventListener('click', function () {
+//         icon.remove()
+//         // 获取选中文本所在的 DOM
+//         const range = window.getSelection().getRangeAt(0);
+//         const containerElement = range.commonAncestorContainer;
+
+//         // TODO：提取用户名
+//         chrome.runtime.sendMessage({
+//             message: "openTab"
+//         })
+
+//         // 取消选择的文字
+//         if (window.getSelection) {
+//             if (window.getSelection().empty) {  // Chrome
+//                 window.getSelection().empty();
+//             } else if (window.getSelection().removeAllRanges) {  // Firefox
+//                 window.getSelection().removeAllRanges();
+//             }
+//         }
+
+//     });
+// }
