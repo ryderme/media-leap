55a43159422ee19fbcc07c55698531a4ae9b2650
Ryder <155456337+ryderme@users.noreply.github.com>
2024-01-12 17:24:10 +0800

    新增跳转ins和facebook的功能
    产品功能精简，删掉了popup的方式，只留了contextMenu的方式

--- background.js
+++ background.js
@@ -1,33 +1,78 @@
-/**
- * 废弃功能
- */
-// chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
-//   if (request.message === "getTwitterUsername") {
-//     let link = "https://instagram.com/" + request.username
-//     console.log(link)
-//   }else if(request.message === "openTab"){
-//     chrome.tabs.create({ url: "https://www.google.com" });
-//   }
-// })
-
+const X_ID = "X";
+const FACEBOOK_ID = "facebook";
+const INSTAGRAM_ID = "instagram";
 
 chrome.runtime.onInstalled.addListener(() => {
-  chrome.contextMenus.create({
-    id: "mediaLeap",
+  let contexts = ["link"];
+
+  let mediaLeapMenu = chrome.contextMenus.create({
     title: "Media Leap",
-    contexts: ["link"]
+    id: "mediaLeap",
+    contexts: contexts
   });
+
+  chrome.contextMenus.create({
+    title: "X",
+    parentId: mediaLeapMenu,
+    id: X_ID,
+    contexts: contexts
+  });
+
+  chrome.contextMenus.create({
+    title: "Instagram",
+    parentId: mediaLeapMenu,
+    id: INSTAGRAM_ID,
+    contexts: contexts
+  });
+
+  chrome.contextMenus.create({
+    title: "Facebook",
+    parentId: mediaLeapMenu,
+    id: FACEBOOK_ID,
+    contexts: contexts
+  })
 });
 
 chrome.contextMenus.onClicked.addListener((info, tab) => {
-  if (info.menuItemId === "mediaLeap") {
-    let twitterUsername = extractTwitterUsername(info.linkUrl)
-    let instagramUrl = `https://instagram.com/${twitterUsername}`;
-    chrome.tabs.create({ url: instagramUrl });
+  let username = extractUsername(info.linkUrl);
+  if (!username) {
+    return;
   }
+
+  let jumpUrl;
+  switch (info.menuItemId) {
+    case X_ID:
+      jumpUrl = `https://twitter.com/${username}`;
+      break;
+    case INSTAGRAM_ID:
+      jumpUrl = `https://instagram.com/${username}`;
+      break;
+    case FACEBOOK_ID:
+      jumpUrl = `https://facebook.com/${username}`;
+      break;
+    default:
+      return;
+  }
+  chrome.tabs.create({ url: jumpUrl });
 });
 
-function extractTwitterUsername(url) {
-  let match = url.match(/twitter\.com\/([a-zA-Z0-9_]+)/);
-  return match ? match[1] : '';
+
+function extractUsername(url) {
+  let username;
+  // 正则表达式匹配 Instagram 用户名
+  const instagramPattern = /instagram\.com\/([A-Za-z0-9._]+)/;
+  // 正则表达式匹配 Facebook 用户名
+  const facebookPattern = /facebook\.com\/([A-Za-z0-9._]+)/;
+  // 正则表达式匹配 Twitter 用户名
+  const twitterPattern = /twitter\.com\/([A-Za-z0-9_]+)/;
+
+  if (instagramPattern.test(url)) {
+    username = url.match(instagramPattern)[1];
+  } else if (facebookPattern.test(url)) {
+    username = url.match(facebookPattern)[1];
+  } else if (twitterPattern.test(url)) {
+    username = url.match(twitterPattern)[1];
+  } 
+
+  return username;
 }
